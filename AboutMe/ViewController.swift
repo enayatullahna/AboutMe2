1d94bba5063a40ada8b88944a2afd92bf9b38896
Enayatullah Naseri <ena.nas@icloud.com>
2019-04-26 10:39:38 -0700

    Remove placeholder labels at runtime

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -17,11 +17,18 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //This is one way to hide it
+ //       self.nameLabel.isHidden = true
+ //       self.hobiesLable.isHidden = true
+        self.nameLabel.text = "" // or you can set it as nil because its optional
+        self.hobiesLable.text = ""
         
     }
 
     @IBAction func introduceSelfButtonTapped(_ sender: Any) {
+        // this is will show it
+//        self.nameLabel.isHidden = false
+//        self.hobiesLable.isHidden = false
         self.nameLabel.text = "Enayatullah Na"
         self.hobiesLable.text = "Basketball, Cricket and Football"
     }
