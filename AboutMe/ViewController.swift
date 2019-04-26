13474cbd80c3fbbc02a8f2f0a75d7df86045ad7a
Enayatullah Naseri <ena.nas@icloud.com>
2019-04-26 10:22:50 -0700

    Add Outlets to ViewController.swift files

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var hobiesLable: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
 
